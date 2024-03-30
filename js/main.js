b734409527e3ff8d1e2b9956bc5933f524e6211a
VedantCMS <vedantgupta.cms@gmail.com>
2024-03-30 15:38:07 +0530

    SUNITAGANDHI FIRST PUSHED

---
+++ js/main.js
@@ -0,0 +1,64 @@
+(function ($) {
+    $(function () {
+
+        $('#navbar-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+
+        $('#navbar-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+            $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+            e.stopPropagation();
+        });
+
+        $('html').click(function () {
+            $('.navbar-dropdown').hide();
+        });
+    });
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('scroll', function () {
+
+        if (window.scrollY > 200) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+
+// Header JS Ends
+
+$('.hero-fading-slide').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    animateOut: 'fadeOut',
+    navText: ['<i class="fa-solid fa-arrow-up"></i>', '<i class="fa-solid fa-arrow-down"></i>'],
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
