ebf8c68b86e08c580ce26108da90fa0deaa69e20
VedantCMS <vedantgupta.cms@gmail.com>
2024-04-01 13:49:31 +0530

    pushed2

--- js/main.js
+++ js/main.js
@@ -62,3 +62,25 @@ $('.hero-fading-slide').owlCarousel({
         }
     }
 })
+
+$('.country-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    autoplay: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
